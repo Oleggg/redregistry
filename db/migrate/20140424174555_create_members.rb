e2692966c3c61c1de9328eae33d9977bcf60f6c4
oleg <oleg@oleg-desktop.(none)>
2014-04-28 21:55:07 +0400

    member model edit

--- db/migrate/20140424174555_create_members.rb
+++ db/migrate/20140424174555_create_members.rb
@@ -2,12 +2,18 @@ class CreateMembers < ActiveRecord::Migration
   def self.up
     create_table :members do |t|
       t.references :region_committee, :null => false
-      t.text :mission, :null => false
+      t.text :mission
       t.date :entry_date
       t.string :membershipcard_number, :null => false
       t.string :phone
       t.string :post
+      t.string :party_post
+      t.boolean :is_distributor, :default => 0
       t.text :note
+      t.string :photo_file_name
+      t.string :photo_content_type
+      t.integer :photo_file_size
+      t.datetime :photo_updated_at
       t.timestamps
     end
   end
